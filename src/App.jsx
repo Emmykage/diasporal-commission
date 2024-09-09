4210cfd89c38ee1674d7970a3cc4fd58a1670063
morris menanya <emmiemenz@gmail.com>
2024-09-09 17:16:09 +0100

    initialize repo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+
+
+        </Routes>
+                        
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className=''>
+        <a href="">ABAccount  </a>
+        <ul>
+        <li><a href=""> Home </a></li>
+        <li><a href=""> About Us </a></li>
+        <li><a href=""> Service </a></li>
+        <li><a href=""> Home </a></li>
+        </ul>
+        
+    </nav>
+  )
+}
+
+export default Nav
